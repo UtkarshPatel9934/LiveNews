57594b265833fcc2ebb1db29b3104ceb48df08fa
utkarsh <90375318+UtkarshPatel9934@users.noreply.github.com>
2022-05-06 13:52:31 -0400

    Setup the state and props

--- livenews/src/components/NewsItem.js
+++ livenews/src/components/NewsItem.js
@@ -5,22 +5,23 @@ export class NewsItem extends Component {
   static propTypes = {
 
   }
+
 // in the class based components we get props by using  "this" keyword.
   render() {
 
 	// this is called destructuring in the Js so that props which gets title and description gets divided and splits the title into the tittle and description into description
-	let {title, description} = this.props
+	let {title, description, urlToImage, url} = this.props
 
     return (
 
 	<>
       <div>
-	<div className="card" style={{'width': '18rem'}}>
-		<img src="" className="card-img-top" alt="..." />
+	<div className="card" style={{width: "18rem"}}>
+		<img src={urlToImage} className="card-img-top" alt="..." urlToImage="https://www.wfsb.com/2022/05/06/entire-state-medium-or-high-category-covid-levels/" />
 			<div className="card-body">
-				<h5 className="card-title">{this.props.title}</h5>
-				<p className="card-text">{this.props.description}</p>
-				<a href="/newsdetail" className="btn btn-primary">Go somewhere</a>
+				<h5 className="card-title">{title}</h5>
+				<p className="card-text">{description}</p>
+				<a href={url} className="btn btn-primary">View Detail</a>
 			</div>
 		</div>
       </div>
