612205f77f99400dfac661ba609293cb753b62d9
utkarsh <90375318+UtkarshPatel9934@users.noreply.github.com>
2022-05-06 13:00:16 -0400

    Add Components to the App

--- livenews/src/App.js
+++ livenews/src/App.js
@@ -2,17 +2,24 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
 
 export default class App extends Component {
   // Note: we dont't have to decalare the variable using let const and var in it....
-  c = "John";
+  // c = "John";
   // if we want to resolve the value in the c so we have to use "this" keyword in it
 
+  // 1. firstly it cmpiles the JSX code 
+  // 2. after it render the code to the page
+
   render() {
+    {/* <h1>This is Class Based Components: {this.c}</h1> */}
     return (
-      <div>
-        <h1>This is Class Based Components: {this.c}</h1>
-      </div>
+         <div>
+           <Navbar />
+           <News/>
+         </div>
     )
   }
 }
---
+++ livenews/src/components/News.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export class News extends Component {
+  static propTypes = {}
+
+  render() {
+    return (
+	    <>
+		<div>This is a news component</div>
+		<NewsItem />
+		<NewsItem />
+		<NewsItem />
+		<NewsItem />
+		<NewsItem />
+      	    </>
+    )
+  }
+}
+
+export default News
