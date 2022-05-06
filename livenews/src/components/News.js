e02b39d27703ed426e30c08ea2fbc0a69720cdb3
utkarsh <90375318+UtkarshPatel9934@users.noreply.github.com>
2022-05-06 13:26:57 -0400

    Setup the components layout and sample json file

--- livenews/src/components/News.js
+++ livenews/src/components/News.js
@@ -8,12 +8,21 @@ export class News extends Component {
   render() {
     return (
 	    <>
-		<div>This is a news component</div>
-		<NewsItem />
-		<NewsItem />
-		<NewsItem />
-		<NewsItem />
-		<NewsItem />
+		<div className='container my-3 text-center'>This is a news component
+		<h1>Live News - Breaking news at your finger tips</h1>
+		{/* we can siply pass the title and description in it. and it will reflects changes into the NewsItem cards section */}
+			<div className="row">
+				<div className="col-md-4">
+					<NewsItem title="This is 1st title" description="This is my 1st description" />
+				</div>
+				<div className="col-md-4">
+					<NewsItem title="This is 1st title" description="This is my 1st description" />
+				</div>
+				<div className="col-md-4">
+					<NewsItem title="This is 1st title" description="This is my 1st description" />
+				</div>
+			</div>
+		</div>
       	    </>
     )
   }
--- livenews/src/components/NewsItem.js
+++ livenews/src/components/NewsItem.js
@@ -5,12 +5,26 @@ export class NewsItem extends Component {
   static propTypes = {
 
   }
-
+// in the class based components we get props by using  "this" keyword.
   render() {
+
+	// this is called destructuring in the Js so that props which gets title and description gets divided and splits the title into the tittle and description into description
+	let {title, description} = this.props
+
     return (
+
+	<>
       <div>
-	This is a news item
+	<div className="card" style={{'width': '18rem'}}>
+		<img src="" className="card-img-top" alt="..." />
+			<div className="card-body">
+				<h5 className="card-title">{this.props.title}</h5>
+				<p className="card-text">{this.props.description}</p>
+				<a href="/newsdetail" className="btn btn-primary">Go somewhere</a>
+			</div>
+		</div>
       </div>
+      </>
     )
   }
 }
