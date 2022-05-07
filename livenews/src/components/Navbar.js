f18308c75fbb01a62382ce96dd9dc73c988a08dc
utkarsh <90375318+UtkarshPatel9934@users.noreply.github.com>
2022-05-07 16:42:37 -0400

    fetch the news data from the API

--- livenews/src/components/Navbar.js
+++ livenews/src/components/Navbar.js
@@ -1,15 +1,21 @@
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 
+
+
+ 
 export class Navbar extends Component {
   static propTypes = {}
 
+
+  
+  
   render() {
     return (
 	<>
 	<nav className="navbar navbar-expand-lg navbar-light bg-light">
 	  <div className="container-fluid">
-	    <a className="navbar-brand" href="/">Live News</a>
+	    <a className="navbar-brand text-info" href="/">Live News</a>
 	    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	      <span className="navbar-toggler-icon"></span>
 	    </button>
@@ -18,21 +24,7 @@ export class Navbar extends Component {
 		<li className="nav-item">
 		  <a className="nav-link active" aria-current="page" href="/">Home</a>
 		</li>
-		<li className="nav-item">
-		  <a className="nav-link" href="/about">About</a>
-		</li>
-		<li className="nav-item dropdown">
-		  <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-		    News Section
-		  </a>
-		  <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-		    <li><a className="dropdown-item" href="/">Action</a></li>
-		    <li><hr className="dropdown-divider"/></li>
-		    <li><a className="dropdown-item" href="/">Another action</a></li>
-		    <li><hr className="dropdown-divider"/></li>
-		    <li><a className="dropdown-item" href="/">Something else here</a></li>
-		  </ul>
-		</li>
+
 	      </ul>
 	      <form className="d-flex">
 		<input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
--- livenews/src/components/NewsItem.js
+++ livenews/src/components/NewsItem.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 
 export class NewsItem extends Component {
   static propTypes = {
-
+	
   }
 
 // in the class based components we get props by using  "this" keyword.
@@ -15,12 +15,12 @@ export class NewsItem extends Component {
     return (
 
 	<>
-	<div className="card" style={{width: "18rem"}}>
-		<img src={urlToImage} className="card-img-top" alt="..."/>
+	<div className="card" style={{"marginBottom": '20px'}}>
+		<img src={urlToImage} style={{"height": "250px"}} className="card-img-top" alt="..."/>
 			<div className="card-body">
 				<h5 className="card-title">{title}...</h5>
 				<p className="card-text">{description}....</p>
-				<a href={url} target="_blank" className="btn btn-primary">View Detail</a>
+				<a href={url} target="_blank" className="btn btn-dark text-light">View Article</a>
 			</div>
 		</div>
       </>
