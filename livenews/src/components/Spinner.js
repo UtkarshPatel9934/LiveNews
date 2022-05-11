cb9d8e0ed8ed0fdc277bff7cbc3c459734ac06ec
utkarsh <90375318+UtkarshPatel9934@users.noreply.github.com>
2022-05-11 15:57:57 -0400

    Added the Routes to App

--- livenews/src/components/Spinner.js
+++ livenews/src/components/Spinner.js
@@ -10,7 +10,7 @@ export default class Spinner extends Component {
   render() {
     return (
       <div className='text-center'>
-	<img src={loading} alt=""/>
+	    <img src={loading} alt=""/>
       </div>
     )
   }
--- livenews/src/index.js
+++ livenews/src/index.js
@@ -4,10 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+
+// imports the file
+import { BrowserRouter } from "react-router-dom"; 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
